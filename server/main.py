1053e9490997ecfb079b00f012da35034fb98e56
Adam Johnson <adaojohnson902@gmail.com>
2024-02-16 17:56:21 -0700

    added python middleware and imported pokebase after reloading the cache

--- server/main.py
+++ server/main.py
@@ -1,11 +1,32 @@
 ﻿from fastapi import FastAPI
+import pokebase as pb
+from fastapi.middleware.cors import CORSMiddleware
+import sys
+
+
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Allows access from your React app
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 
 @app.get("/")
 async def read_root():
     return {"Hello": "from FastAPI"}
 
-@app.get("/getPokemon")
-async def read_root():
-    return 
+@app.get("/pokemon/{name}")
+async def get_pokemon(name: str):
+    try: 
+        pokemon = pb.pokemon(name.lower())
+        print(pokemon)
+        return {'data': pokemon}
+    except Exception as e:
+        return {'error': str(e)}
+
--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,60 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  interface PokemonData {
+    name: string
+    id: number
+    sprites: {
+      front_default: string;
+    }
+  }
+
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
+  const [pokemon, setPokemon] = useState<PokemonData | null>(null)
+  const [error, setError] = useState('')
+
+  const handleInputChange = (e: any) => {
+    setName(e.target.value)
+    console.log(name)
+  }
+  
+  const searchPokemon = async () => {
+    try {
+      console.log(pokemon)
+      setCount(count + 1)
+      const response = await fetch(`http://localhost:8000/getPokemon${name}`)
+      if (!response.ok) {
+        throw new Error(`Failed to fetch: ${response.status}`)
+      }
+      const data = await response.json()
+      //@ts-ignore
+      setPokemon(data);
+      setError('')
+    } catch (err: any) {
+      setError(err.message)
+      setPokemon(null)
+    }
+  }
   return (
     <>
       <h2>This project is going to help me to build teams when I play through Pokemon games</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Generate Team
-        </button>
+        <input type="text"
+        value={name}
+        onChange={handleInputChange}
+        placeholder='Enter Pokemon Name' />
+        <button onClick={searchPokemon}>Search</button>
         <br />
+        {error && <p>Error: {error}</p>}
+        {pokemon && (
+          <div>
+            <h2>{pokemon.name}</h2>
+          </div>
+        )}
       </div>
-          <div>Teams generated: {count}</div>
+          <div>Pokemon generated: {count}</div>
           <br />
       <a href="https://pokeapi.co/">PokeAPI</a>
     </>
